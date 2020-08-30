8ec202d5a5de306ee6dd3c06ece46254b70b8290
Lyudmyla Tkachenko <luda_tkach@yahoo.com>
2020-08-30 13:44:42 -0700

    Adding open different domains in different tabs

--- test/specs/testAnimals.spec.js
+++ test/specs/testAnimals.spec.js
@@ -1,4 +1,4 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 import axios from 'axios';
 
 let userEmail;
@@ -7,85 +7,100 @@ let dogImage;
 let foxImage;
 
 describe('TEST TASK', () => {
-
   before('go to getnada.com', () => {
     browser.url('https://getnada.com');
     browser.maximizeWindow();
   });
 
-  it('should be a right title ', () => {
+  it('should have a right title ', () => {
     expect(browser.getTitle()).eq('Nada - temp mail - fast and free');
   });
 
   it('should create an email', () => {
     browser.$('//i[@class="icon-plus"]').click();
     let userName = browser.$('//input[@class="user_name"]').getValue();
+    let d = new Date();
+    userName = userName + d.getTime();
     userEmail = userName + '@getnada.com';
+    browser.$('//input[@class="user_name"]').keys("Control", "a");
+    browser.$('//input[@class="user_name"]').setValue(userName);
+
     browser.$('//a[@class="button success"]').click();
     expect(browser.$('//a[@class=" is-active"]//span').getText()).eq(userEmail);
   });
 
-
-  it('should get cat image url', async  () => {
-     const result = await axios({
+  it('should get API cat image url', async () => {
+    const result = await axios({
       method: 'get',
       url: 'http://aws.random.cat/meow',
     })
       .then(res => res.data)
-      .catch(err => err.response.data)
+      .catch(err => err.response.data);
     catImage = result.file;
     expect(result.file).to.match(/.jpe?g|.png|.mp4|.gif|.webm$/i);
   });
 
-  it('should get dog image url', async  () => {
+  it('should get API dog image url', async () => {
     const result = await axios({
       method: 'get',
       url: 'http://random.dog/woof.json',
     })
       .then(res => res.data)
-      .catch(err => err.response.data)
+      .catch(err => err.response.data);
     dogImage = result.url;
     expect(result.url).to.match(/.jpe?g|.png|.mp4|.gif|.webm$/i);
   });
 
-  it('should get fox image url', async  () => {
-        const result = await axios({
+  it('should get API fox image url', async () => {
+    const result = await axios({
       method: 'get',
       url: 'http://randomfox.ca/floof/',
     })
       .then(res => res.data)
-      .catch(err => err.response.data)
+      .catch(err => err.response.data);
     foxImage = result.image;
     expect(result.image).to.match(/.jpe?g|.png|.mp4|.gif|.webm$/i);
   });
 
   it('should login in gmail', () => {
-    browser.url('https://mail.google.com');
+    browser.newWindow('https://mail.google.com');
+    browser.maximizeWindow();
     browser.$('//input[@id="identifierId"]').setValue('lt364226@gmail.com');
     browser.$('//div[@class="VfPpkd-RLmnJb"]').click();
-    browser.waitUntil( () => browser.$('//div[@id="profileIdentifier"]').isDisplayed());
-    //browser.$('//h1//span')
+    browser.waitUntil(() => browser.$('//input[@type="password"]').isDisplayed());
+    //browser.waitUntil( () => browser.$('//div[@id="profileIdentifier"]').isDisplayed());
     browser.$('//input[@class="whsOnd zHQkBf"]').setValue('lt112233');
     browser.$('//div[@class="VfPpkd-RLmnJb"]').click();
-    browser.waitUntil( () => browser.$('//img[@class="gb_va"]').isDisplayed());
+    browser.waitUntil(() => browser.$('//img[@class="gb_va"]').isDisplayed());
+    expect(browser.$('//div[@class="T-I T-I-KE L3"]').isClickable()).eq(true);
   });
 
   it('should create letter in gmail account and send it to another email', () => {
     browser.$('//div[@class="T-I T-I-KE L3"]').click();
-    browser.$('//textarea[@id=":8y"]').setValue(userEmail);
-    //browser.$('//div[@id=":ac"]').setValue(userEmail);
-    browser.$('//div[@id=":9l"]').setValue(catImage + "\n" + dogImage + "\n" + foxImage);
-    browser.$('//div[@id=":86"]').click();
-    browser.waitUntil( () => browser.$('//span[@class="bAq"]').isDisplayed());
-    expect(browser.$('//span[@class="bAq"]').isDisplayed()).eq(true);
+    browser.$('//textarea[@class="vO"]').setValue(userEmail);
+    browser
+      .$('//div[@class="Am Al editable LW-avf tS-tW"]')
+      .setValue(catImage + '\n' + dogImage + '\n' + foxImage);
+    browser.$('//div[text()="Send"]').click();
+    browser.waitUntil(() => browser.$('//span[text()="Message sent."]').isDisplayed());
+    expect(browser.$('//div[@class="vh"]//span[@class="aT"]').isDisplayed()).eq(true);
   });
 
-  it('should print email', () => {
-    console.log('######################################');
-    console.log(userEmail);
-    console.log('######################################');
+  it('should check email with 3 url in getnada email box', () => {
+    browser.switchWindow('https://getnada.com/');
+    browser.waitUntil(() => browser.$('//li[@class="msg_item"]').isClickable(), { timeout: 120000 });
+    browser.$('//li[@class="msg_item"]').click();
+    browser.waitUntil(() => browser.$('//iframe[@id="idIframe"]').isDisplayed());
+    const emailFrame = browser.$('//iframe[@id="idIframe"]');
+    browser.switchToFrame(emailFrame);
+    browser.waitUntil(() => browser.$('//a[text()="' + catImage + '"]').isDisplayed());
+    browser.waitUntil(() => browser.$('//a[text()="' + dogImage + '"]').isDisplayed());
+    browser.waitUntil(() => browser.$('//a[text()="' + foxImage + '"]').isDisplayed());
+    let catUrl = browser.$('//div[@dir="ltr"]/a').getAttribute('href');
+    let dogUrl = browser.$('(//div[@dir="ltr"]/div/a)[1]').getAttribute('href');
+    let foxUrl = browser.$('(//div[@dir="ltr"]/div/a)[2]').getAttribute('href');
+    expect(catUrl).eq(catImage);
+    expect(dogUrl).eq(dogImage);
+    expect(foxUrl).eq(foxImage);
   });
-
-
-
 });
